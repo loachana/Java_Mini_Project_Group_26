829b069e2567cf9e6fd40b4ee6b502775386a0ea
unknown <lochanaeg@gmail.com>
2025-04-10 17:48:38 +0530

    Being born

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void greeting() {
+
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("The Student Management System");
+        System.out.println("------------------------------");
+        System.out.println("1 - To view current database");
+        System.out.println("2 - To add new entry");
+        System.out.println("3 - To remove existing entry");
+
+
+
+    }
+}
