5812294c64ae49433dd9c1dd0955d280c1336152
unknown <lochanaeg@gmail.com>
2025-04-13 21:31:26 +0530

    Update-1.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -117,19 +117,87 @@ public class Main {
 
     public static String[] DataEntryWindow() {
 
+        String filePath = "database.csv";
 
-        String[] DataArray = new String[2];
+        // creating File object with the file location specified
+        File file = new File(filePath);
+
+        String LastID = "1";
+
+        try {
+            // creating FileReader object with file as parameter
+            FileReader fileReader = new FileReader(file);
+
+            // creating CSVReader object using FileReader object
+            CSVReader csvReader = new CSVReader(fileReader);
+
+            // creating the list (containing both header and data) using String array
+            List<String[]> data = csvReader.readAll();
+
+            //System.out.println(data.getLast()[0]);
+
+            LastID = data.getLast()[0];
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvException e) {
+            throw new RuntimeException(e);
+        }
+
+        //System.out.println(LastID);
+
+        String[] DataArray = new String[3];
 
         Scanner scan = new Scanner(System.in);
 
+        //int IntID = 1;
+
+        int LastIDInt = Integer.parseInt(LastID);
+
+        //System.out.println(LastIDInt+4);
+
+        int NewIDInt = LastIDInt + 1;
+
+        String NewID = String.valueOf(NewIDInt);
+
+        DataArray[0] = NewID;
+
         System.out.print("Name: ");
-        DataArray[0] = scan.nextLine();
+        DataArray[1] = scan.nextLine();
 
         System.out.print("Age: ");
-        DataArray[1] = scan.nextLine();
+        DataArray[2] = scan.nextLine();
 
         return DataArray;
     }
+
+    /*
+    public static String[] DataEntryWindow() {
+
+
+        String[] DataArray = new String[3];
+
+        Scanner scan = new Scanner(System.in);
+
+        int IntID = 1;
+
+        String StringID = String.valueOf(IntID);
+
+        DataArray[0] = StringID;
+
+        System.out.print("Name: ");
+        DataArray[1] = scan.nextLine();
+
+        System.out.print("Age: ");
+        DataArray[2] = scan.nextLine();
+
+        return DataArray;
+    } */
+
+
 
     public static int InterfaceMainMenu() {
 
@@ -185,10 +253,11 @@ public class Main {
 
             // printing data
             at.addRule();
-            at.addRow("NAME", "AGE");
+            at.addRow("ID", "NAME", "AGE");
             for (String[] row : data) {
+
                 at.addRule();
-                at.addRow(row[0], row[1]);
+                at.addRow(row[0], row[1], row[2]);
                 //at.addRule();
                 //System.out.println(row[0] + row[1]);
 
@@ -210,7 +279,26 @@ public class Main {
 
 
 
-    public static void Writer(String name, String age) {
+
+    public static void Writer(String ID, String name, String age) {
+
+        String csvFile = "database.csv";
+
+        try (CSVWriter writer = new CSVWriter(new FileWriter(csvFile, true))) {
+
+            String[] Details = {ID, name, age};
+
+            writer.writeNext(Details);
+
+            System.out.println("Data saved\n");
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /*public static void Writer(String name, String age) {
 
         String csvFile = "database.csv";
 
@@ -226,8 +314,7 @@ public class Main {
             e.printStackTrace();
         }
 
-    }
-
+    }*/
 
     public static void main(String[] args) throws CsvException, FileNotFoundException {
 
@@ -247,7 +334,7 @@ public class Main {
                     break;
                 case 2:
                     String[] DataList = DataEntryWindow();
-                    Writer(DataList[0], DataList[1]);
+                    Writer(DataList[0], DataList[1], DataList[2]);
                     break;
                 case 3:
                     DeleteData();
